263c97ae378ac7f7ffc22eb25db67eb4538c0197
steven.yan <steven.yan@gmail.com>
2014-07-10 20:39:34 +0800

    Initial Commit

---
+++ e-HeathyTests/e_HeathyTests.m
@@ -0,0 +1,34 @@
+//
+//  e_HeathyTests.m
+//  e-HeathyTests
+//
+//  Created by steven on 7/10/14.
+//  Copyright (c) 2014 steven. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface e_HeathyTests : XCTestCase
+
+@end
+
+@implementation e_HeathyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
